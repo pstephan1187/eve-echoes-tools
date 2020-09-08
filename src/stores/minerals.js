7a5b2cd60682cbe9f44ca4980fd5b698f4dd5311
Patrick Stephan <pstephan1187@gmail.com>
2020-09-07 21:34:39 -0500

    Updated ore and mineral values.

--- src/stores/minerals.js
+++ src/stores/minerals.js
@@ -1,12 +1,12 @@
-export const lastUpdateTimestamp = [2020, 9, 5, 22, 36];
+export const lastUpdateTimestamp = [2020, 9, 7, 21, 34];
 
 export const minerals = [
   { label: "Tritanium", volume: 0.01, value: 2 },
-  { label: "Pyerite", volume: 0.01, value: 22 },
-  { label: "Mexallon", volume: 0.01, value: 38 },
-  { label: "Isogen", volume: 0.01, value: 127 },
-  { label: "Nocxium", volume: 0.01, value: 1203 },
-  { label: "Zydrine", volume: 0.01, value: 900 },
-  { label: "Megacyte", volume: 0.01, value: 3400 },
-  { label: "Morphite", volume: 0.01, value: 3001 },
+  { label: "Pyerite", volume: 0.01, value: 24 },
+  { label: "Mexallon", volume: 0.01, value: 37 },
+  { label: "Isogen", volume: 0.01, value: 136 },
+  { label: "Nocxium", volume: 0.01, value: 1258 },
+  { label: "Zydrine", volume: 0.01, value: 866 },
+  { label: "Megacyte", volume: 0.01, value: 3226 },
+  { label: "Morphite", volume: 0.01, value: 2208 },
 ];
--- src/stores/ores.js
+++ src/stores/ores.js
@@ -4,27 +4,27 @@ export const ORE_TYPE_SPECIAL = "Special";
 export const ORE_TYPE_RARE = "Rare";
 export const ORE_TYPE_PRECIOUS = "Precious";
 
-export const lastUpdateTimestamp = [2020, 9, 5, 22, 36];
+export const lastUpdateTimestamp = [2020, 9, 7, 21, 34];
 
 export const ores = [
-  { label: "Veldspar", type: ORE_TYPE_COMMON, minSec: '-1.0', maxSec: '1.0', volume: 0.1, value: 6 },
-  { label: "Scordite", type: ORE_TYPE_COMMON, minSec: '-1.0', maxSec: '1.0', volume: 0.15, value: 12 },
-  { label: "Plagioclase", type: ORE_TYPE_COMMON, minSec: '0.3', maxSec: '0.8', volume: 0.35, value: 22 },
+  { label: "Veldspar", type: ORE_TYPE_COMMON, minSec: '-1.0', maxSec: '1.0', volume: 0.1, value: 7 },
+  { label: "Scordite", type: ORE_TYPE_COMMON, minSec: '-1.0', maxSec: '1.0', volume: 0.15, value: 14 },
+  { label: "Plagioclase", type: ORE_TYPE_COMMON, minSec: '0.3', maxSec: '0.8', volume: 0.35, value: 23 },
 
-  { label: "Omber", type: ORE_TYPE_UNCOMMON, minSec: '0.3', maxSec: '0.6', volume: 0.6, value: 50 },
-  { label: "Kernite", type: ORE_TYPE_UNCOMMON, minSec: '0.1', maxSec: '0.6', volume: 1.2, value: 100 },
-  { label: "Pyroxeres", type: ORE_TYPE_UNCOMMON, minSec: '-1.0', maxSec: '0.4', volume: 1.5, value: 260 },
-  { label: "Dark Ochre", type: ORE_TYPE_UNCOMMON, minSec: '-1.0', maxSec: '0.4', volume: 1.6, value: 276 },
+  { label: "Omber", type: ORE_TYPE_UNCOMMON, minSec: '0.3', maxSec: '0.6', volume: 0.6, value: 45 },
+  { label: "Kernite", type: ORE_TYPE_UNCOMMON, minSec: '0.1', maxSec: '0.6', volume: 1.2, value: 101 },
+  { label: "Pyroxeres", type: ORE_TYPE_UNCOMMON, minSec: '-1.0', maxSec: '0.4', volume: 1.5, value: 267 },
+  { label: "Dark Ochre", type: ORE_TYPE_UNCOMMON, minSec: '-1.0', maxSec: '0.4', volume: 1.6, value: 270 },
 
-  { label: "Gneiss", type: ORE_TYPE_SPECIAL, minSec: '-1.0', maxSec: '0.4', volume: 2, value: 369 },
-  { label: "Hemorphite", type: ORE_TYPE_SPECIAL, minSec: '-1.0', maxSec: '0.2', volume: 3, value: 465 },
-  { label: "Spodumain", type: ORE_TYPE_SPECIAL, minSec: '-1.0', maxSec: '0.2', volume: 3.2, value: 610 },
+  { label: "Gneiss", type: ORE_TYPE_SPECIAL, minSec: '-1.0', maxSec: '0.4', volume: 2, value: 400 },
+  { label: "Hemorphite", type: ORE_TYPE_SPECIAL, minSec: '-1.0', maxSec: '0.2', volume: 3, value: 500 },
+  { label: "Spodumain", type: ORE_TYPE_SPECIAL, minSec: '-1.0', maxSec: '0.2', volume: 3.2, value: 665 },
 
-  { label: "Hedbergite", type: ORE_TYPE_RARE, minSec: '-1.0', maxSec: '0.0', volume: 3, value: 550 },
-  { label: "Jaspet", type: ORE_TYPE_RARE, minSec: '-1.0', maxSec: '0.0', volume: 4, value: 716 },
-  { label: "Crokite", type: ORE_TYPE_RARE, minSec: '-1.0', maxSec: '-0.2', volume: 6.4, value: 1001 },
+  { label: "Hedbergite", type: ORE_TYPE_RARE, minSec: '-1.0', maxSec: '0.0', volume: 3, value: 661 },
+  { label: "Jaspet", type: ORE_TYPE_RARE, minSec: '-1.0', maxSec: '0.0', volume: 4, value: 800 },
+  { label: "Crokite", type: ORE_TYPE_RARE, minSec: '-1.0', maxSec: '-0.2', volume: 6.4, value: 1201 },
 
-  { label: "Arkonor", type: ORE_TYPE_PRECIOUS, minSec: '-1.0', maxSec: '-0.6', volume: 6.4, value: 856 },
-  { label: "Bistot", type: ORE_TYPE_PRECIOUS, minSec: '-1.0', maxSec: '-0.4', volume: 6.4, value: 1048 },
-  { label: "Mercoxit", type: ORE_TYPE_PRECIOUS, minSec: '-1.0', maxSec: '-0.8', volume: 8, value: 541 },
+  { label: "Arkonor", type: ORE_TYPE_PRECIOUS, minSec: '-1.0', maxSec: '-0.6', volume: 6.4, value: 1005 },
+  { label: "Bistot", type: ORE_TYPE_PRECIOUS, minSec: '-1.0', maxSec: '-0.4', volume: 6.4, value: 910 },
+  { label: "Mercoxit", type: ORE_TYPE_PRECIOUS, minSec: '-1.0', maxSec: '-0.8', volume: 8, value: 713 },
 ]
