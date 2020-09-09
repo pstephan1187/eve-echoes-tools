2de812ff3b11b72c02ea035bf3cc521d07e71858
Patrick Stephan <pstephan1187@gmail.com>
2020-09-09 07:47:21 -0500

    Updates ore and mineral values.

--- src/stores/minerals.js
+++ src/stores/minerals.js
@@ -1,12 +1,12 @@
-export const lastUpdateTimestamp = [2020, 9, 7, 21, 34];
+export const lastUpdateTimestamp = [2020, 9, 9, 7, 45];
 
 export const minerals = [
   { label: "Tritanium", volume: 0.01, value: 2 },
-  { label: "Pyerite", volume: 0.01, value: 24 },
-  { label: "Mexallon", volume: 0.01, value: 37 },
-  { label: "Isogen", volume: 0.01, value: 136 },
-  { label: "Nocxium", volume: 0.01, value: 1258 },
-  { label: "Zydrine", volume: 0.01, value: 866 },
-  { label: "Megacyte", volume: 0.01, value: 3226 },
-  { label: "Morphite", volume: 0.01, value: 2208 },
+  { label: "Pyerite", volume: 0.01, value: 26 },
+  { label: "Mexallon", volume: 0.01, value: 40 },
+  { label: "Isogen", volume: 0.01, value: 141 },
+  { label: "Nocxium", volume: 0.01, value: 1305 },
+  { label: "Zydrine", volume: 0.01, value: 1150 },
+  { label: "Megacyte", volume: 0.01, value: 3623 },
+  { label: "Morphite", volume: 0.01, value: 2505 },
 ];
