e8d2173a56d459da9661612c4282aa580472c35c
Patrick Stephan <pstephan1187@gmail.com>
2020-09-17 07:56:08 -0500

    Updated market values.

--- src/MarketValues.js
+++ src/MarketValues.js
@@ -1,37 +1,37 @@
 import { toUniversalTime } from './utils';
 
-export const lastUpdated = toUniversalTime(2020, 9, 16, 8, 29);
+export const lastUpdated = toUniversalTime(2020, 9, 17, 7, 55);
 
 export default {
   // ores
-  veldspar:     { jita:    6 },
-  scordite:     { jita:   17 },
-  plagioclase:  { jita:   28 },
+  veldspar:     { jita:    5 },
+  scordite:     { jita:   20 },
+  plagioclase:  { jita:   41 },
 
-  omber:        { jita:   60 },
-  kernite:      { jita:  145 },
-  pyroxeres:    { jita:  340 },
-  dark_ochre:   { jita:  361 },
+  omber:        { jita:   76 },
+  kernite:      { jita:  171 },
+  pyroxeres:    { jita:  480 },
+  dark_ochre:   { jita:  480 },
 
-  gneiss:       { jita:  520 },
-  hemorphite:   { jita:  703 },
-  spodumain:    { jita:  770 },
+  gneiss:       { jita:  603 },
+  hemorphite:   { jita:  880 },
+  spodumain:    { jita:  860 },
 
-  hedbergite:   { jita:  822 },
-  jaspet:       { jita: 1260 },
-  crokite:      { jita: 1750 },
+  hedbergite:   { jita:  920 },
+  jaspet:       { jita: 1406 },
+  crokite:      { jita: 2010 },
 
-  arkonor:      { jita: 2200 },
-  bistot:       { jita: 1812 },
-  mercoxit:     { jita: 1320 },
+  arkonor:      { jita: 2210 },
+  bistot:       { jita: 2000 },
+  mercoxit:     { jita: 1401 },
 
   // minerals
-  tritanium:    { jita:    2 },
-  pyerite:      { jita:   26 },
-  mexallon:     { jita:   56 },
-  isogen:       { jita:  183 },
-  nocxium:      { jita: 1500 },
-  zydrine:      { jita: 1581 },
-  megacyte:     { jita: 5107 },
-  morphite:     { jita: 6507 },
+  tritanium:    { jita:    2 },// 2
+  pyerite:      { jita:   38 },// 36
+  mexallon:     { jita:   56 },// 55
+  isogen:       { jita:  192 },// 165
+  nocxium:      { jita: 2500 },// 2410
+  zydrine:      { jita: 1901 },// 1500
+  megacyte:     { jita: 4200 },
+  morphite:     { jita: 3110 },
 };
