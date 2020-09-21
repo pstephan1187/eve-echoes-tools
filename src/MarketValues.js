ffe10fac932037c2a8f16322674fc7c744ff53cd
Patrick Stephan <pstephan1187@gmail.com>
2020-09-20 23:44:10 -0500

    Updated market values.

--- src/MarketValues.js
+++ src/MarketValues.js
@@ -1,37 +1,37 @@
 import { toUniversalTime } from './utils';
 
-export const lastUpdated = toUniversalTime(2020, 9, 17, 7, 55);
+export const lastUpdated = toUniversalTime(2020, 9, 20, 23, 43);
 
 export default {
   // ores
-  veldspar:     { jita:    5 },
-  scordite:     { jita:   20 },
-  plagioclase:  { jita:   41 },
+  veldspar:     { jita:    6 },
+  scordite:     { jita:   24 },
+  plagioclase:  { jita:   43 },
 
-  omber:        { jita:   76 },
-  kernite:      { jita:  171 },
-  pyroxeres:    { jita:  480 },
-  dark_ochre:   { jita:  480 },
+  omber:        { jita:   70 },
+  kernite:      { jita:  174 },
+  pyroxeres:    { jita:  550 },
+  dark_ochre:   { jita:  511 },
 
-  gneiss:       { jita:  603 },
-  hemorphite:   { jita:  880 },
-  spodumain:    { jita:  860 },
+  gneiss:       { jita:  710 },
+  hemorphite:   { jita:  912 },
+  spodumain:    { jita: 1063 },
 
-  hedbergite:   { jita:  920 },
-  jaspet:       { jita: 1406 },
-  crokite:      { jita: 2010 },
+  hedbergite:   { jita: 1125 },
+  jaspet:       { jita: 1606 },
+  crokite:      { jita: 2137 },
 
-  arkonor:      { jita: 2210 },
-  bistot:       { jita: 2000 },
-  mercoxit:     { jita: 1401 },
+  arkonor:      { jita: 2300 },
+  bistot:       { jita: 2250 },
+  mercoxit:     { jita: 1455 },
 
   // minerals
   tritanium:    { jita:    2 },// 2
-  pyerite:      { jita:   38 },// 36
-  mexallon:     { jita:   56 },// 55
-  isogen:       { jita:  192 },// 165
-  nocxium:      { jita: 2500 },// 2410
-  zydrine:      { jita: 1901 },// 1500
+  pyerite:      { jita:   39 },// 36
+  mexallon:     { jita:   68 },// 55
+  isogen:       { jita:  176 },// 165
+  nocxium:      { jita: 2230 },// 2410
+  zydrine:      { jita: 1802 },// 1500
   megacyte:     { jita: 4200 },
-  morphite:     { jita: 3110 },
+  morphite:     { jita: 4100 },
 };
