eecf99607f949368b9cd35cc08ea4e184e5ef7fb
Patrick Stephan <pstephan1187@gmail.com>
2020-09-28 07:52:56 -0500

    Updated market values.

--- src/MarketValues.js
+++ src/MarketValues.js
@@ -1,37 +1,37 @@
 import { toUniversalTime } from './utils';
 
-export const lastUpdated = toUniversalTime(2020, 9, 22, 20, 40);
+export const lastUpdated = toUniversalTime(2020, 9, 28, 7, 52);
 
 export default {
   // ores
   veldspar:     { jita:    6 },
-  scordite:     { jita:   23 },
-  plagioclase:  { jita:   41 },
+  scordite:     { jita:   19 },
+  plagioclase:  { jita:   32 },
 
-  omber:        { jita:   68 },
-  kernite:      { jita:  154 },
-  pyroxeres:    { jita:  490 },
-  dark_ochre:   { jita:  490 },
+  omber:        { jita:   56 },
+  kernite:      { jita:  139 },
+  pyroxeres:    { jita:  456 },
+  dark_ochre:   { jita:  486 },
 
-  gneiss:       { jita:  615 },
-  hemorphite:   { jita:  742 },
-  spodumain:    { jita: 1060 },
+  gneiss:       { jita:  501 },
+  hemorphite:   { jita:  866 },
+  spodumain:    { jita:  936 },
 
-  hedbergite:   { jita: 1160 },
-  jaspet:       { jita: 1505 },
-  crokite:      { jita: 2150 },
+  hedbergite:   { jita:  750 },
+  jaspet:       { jita: 1363 },
+  crokite:      { jita: 1805 },
 
-  arkonor:      { jita: 2211 },
-  bistot:       { jita: 2305 },
-  mercoxit:     { jita: 2000 },
+  arkonor:      { jita: 1950 },
+  bistot:       { jita: 2050 },
+  mercoxit:     { jita: 1600 },
 
   // minerals
-  tritanium:    { jita:    3 },// 2
-  pyerite:      { jita:   37 },// 36
-  mexallon:     { jita:   56 },// 55
-  isogen:       { jita:  153 },// 165
-  nocxium:      { jita: 2050 },// 2410
-  zydrine:      { jita: 1800 },// 1500
-  megacyte:     { jita: 4501 },
-  morphite:     { jita: 4100 },
+  tritanium:    { jita:    2 },// 2
+  pyerite:      { jita:   34 },// 36
+  mexallon:     { jita:   41 },// 55
+  isogen:       { jita:  132 },// 165
+  nocxium:      { jita: 2001 },// 2410
+  zydrine:      { jita: 1717 },// 1500
+  megacyte:     { jita: 3200 },
+  morphite:     { jita: 3600 },
 };
